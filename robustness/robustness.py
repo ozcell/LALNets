1409e12979d0eb7bb33410d0842456be5de7babb
ozcell <ozsel@ozsel-mac.local>
2017-04-02 04:35:10 -0400

    robustness 1.1

--- robustness/robustness.py
+++ robustness/robustness.py
@@ -28,7 +28,7 @@ import matplotlib.cm as cm
 import matplotlib.gridspec as gridspec
 import time
 
-
+from models import define_cnn, define_mlp
 
 np.random.seed(1337)
 
@@ -51,55 +51,6 @@ X_test /= 255
 nb_classes = 10
 Y_train = np_utils.to_categorical(y_train, nb_classes)
 Y_test = np_utils.to_categorical(y_test, nb_classes)
-
-def define_cnn(input_shape, nb_classes, cnn_type=1, nb_filters = 32, nb_pool = 2, nb_conv = 3):
-    model = Sequential()
-    model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
-                            activation='relu', border_mode='same', dim_ordering='tf',
-                            input_shape=input_shape))
-    model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
-                            activation='relu', border_mode='same', dim_ordering='tf'))
-    model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool), dim_ordering='tf'))
-    model.add(Dropout(0.25))
-
-    if cnn_type>1:
-        model.add(Convolution2D(nb_filters*2, nb_conv, nb_conv,
-                               activation='relu', border_mode='same', dim_ordering='tf'))
-        model.add(Convolution2D(nb_filters*2, nb_conv, nb_conv,
-                               activation='relu', border_mode='same', dim_ordering='tf'))
-        model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool), dim_ordering='tf'))
-        model.add(Dropout(0.25))
-
-    if cnn_type>2:
-        model.add(Convolution2D(nb_filters*4, nb_conv, nb_conv,
-                            activation='relu', border_mode='same', dim_ordering='tf'))
-        model.add(Convolution2D(nb_filters*4, nb_conv, nb_conv,
-                            activation='relu', border_mode='same', dim_ordering='tf'))
-        model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool), dim_ordering='tf'))
-        model.add(Dropout(0.25))
-
-    model.add(Flatten())
-    model.add(Dense(2048, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(nb_classes, activation='softmax'))
-
-    return model
-
-def define_mlp(input_shape, nb_classes):
-
-    model = Sequential()
-    model.add(Dense(2048, activation='relu', W_constraint=maxnorm(2.), input_shape=input_shape))
-    model.add(Dropout(0.50))
-
-    model.add(Dense(2048, activation='relu', W_constraint=maxnorm(2.)))
-    model.add(Dropout(0.50))
-
-    model.add(Dense(2048, activation='relu', W_constraint=maxnorm(2.)))
-    model.add(Dropout(0.50))
-
-    model.add(Dense(nb_classes, activation='relu'))
-
-    return model
 
 nb_clusters = 5
 nb_epoch = 150
