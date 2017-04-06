a30936f3494df4c18263440a1ea74e01d82f6d5e
ozcell <ozsel@ozsel-mac.local>
2017-04-06 19:10:17 -0400

    setup file update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 
 setup(name='phdwork',
-      version='0.1.1',
+      version='0.1.2',
       description='Ozsel Kilinc Ph.D. Work',
       author='Ozsel Kilinc',
       author_email='ozselkilinc@gmail.com',
