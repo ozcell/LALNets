200162eb922c0a5a192e77108d48f46f3f873086
ozcell <ozsel@ozsel-mac.local>
2017-04-06 20:07:48 -0400

    library issues

--- phdwork/acol/__init__.py
+++ phdwork/acol/__init__.py
@@ -2,5 +2,6 @@ from __future__ import absolute_import
 from . import layers
 from . import initializations
 from . import regularizers
+from . import models
 
 __version__ = '0.3.1'
--- phdwork/acol/models.py
+++ phdwork/acol/models.py
@@ -1,16 +1,25 @@
 '''
 
-Model generator for robustness experiments.
+Model generator for ACOL experiments.
 
 '''
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten, Layer
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.constraints import maxnorm
+
+from phdwork.acol.layers.pooling import AveragePooling, MaxPooling
+from phdwork.acol.regularizers import activity_acol
 
 
-def define_cnn(input_shape, nb_classes, cnn_type=1, nb_filters = 32, nb_pool = 2, nb_conv = 3):
+def define_cnn(input_shape, nb_classes, cnn_type=1, conv_params=(32,3,2), hidden_drop = True, acol_en = False,
+               acol_params=(5, 0, 1, 1, 0, 0.000001, AveragePooling, False, 'identity_vstacked', False),
+               truncated = False):
+
+    nb_filters, nb_conv, nb_pool = conv_params
+
+    K, p, c1, c2, c3, c4, AcolPooling, trainable, init, null_node = acol_params
+
     model = Sequential()
     model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
                             activation='relu', border_mode='same', dim_ordering='tf',
@@ -18,7 +27,7 @@ def define_cnn(input_shape, nb_classes, cnn_type=1, nb_filters = 32, nb_pool = 2
     model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
                             activation='relu', border_mode='same', dim_ordering='tf'))
     model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool), dim_ordering='tf'))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.25)) if hidden_drop else model.add(Dropout(0.))
 
     if cnn_type>1:
         model.add(Convolution2D(nb_filters*2, nb_conv, nb_conv,
@@ -26,7 +35,7 @@ def define_cnn(input_shape, nb_classes, cnn_type=1, nb_filters = 32, nb_pool = 2
         model.add(Convolution2D(nb_filters*2, nb_conv, nb_conv,
                                activation='relu', border_mode='same', dim_ordering='tf'))
         model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool), dim_ordering='tf'))
-        model.add(Dropout(0.25))
+        model.add(Dropout(0.25)) if hidden_drop else model.add(Dropout(0.))
 
     if cnn_type>2:
         model.add(Convolution2D(nb_filters*4, nb_conv, nb_conv,
@@ -34,27 +43,23 @@ def define_cnn(input_shape, nb_classes, cnn_type=1, nb_filters = 32, nb_pool = 2
         model.add(Convolution2D(nb_filters*4, nb_conv, nb_conv,
                             activation='relu', border_mode='same', dim_ordering='tf'))
         model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool), dim_ordering='tf'))
-        model.add(Dropout(0.25))
+        model.add(Dropout(0.25)) if hidden_drop else model.add(Dropout(0.))
 
     model.add(Flatten())
     model.add(Dense(2048, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(nb_classes, activation='softmax'))
+    model.add(Dropout(0.5)) if hidden_drop else model.add(Dropout(0.))
 
-    return model
-
-def define_mlp(input_shape, nb_classes):
-
-    model = Sequential()
-    model.add(Dense(2048, activation='relu', W_constraint=maxnorm(2.), input_shape=input_shape))
-    model.add(Dropout(0.50))
-
-    model.add(Dense(2048, activation='relu', W_constraint=maxnorm(2.)))
-    model.add(Dropout(0.50))
-
-    model.add(Dense(2048, activation='relu', W_constraint=maxnorm(2.)))
-    model.add(Dropout(0.50))
-
-    model.add(Dense(nb_classes, activation='relu'))
+    if acol_en == True:
+        model.add(Dense(nb_classes*K, activity_regularizer=activity_acol(c1, c2, c3, c4), name='L-1'))
+        model.add(Dropout(p)) if hidden_drop else model.add(Dropout(0.))
+        if not truncated:
+            model.add(Activation('softmax'))
+            if null_node:
+                model.add(AcolPooling(nb_classes+1, trainable=trainable, init='column_vstacked_nullnode'))
+            else:
+                model.add(AcolPooling(nb_classes, trainable=trainable, init=init))
+    else:
+        model.add(Dense(nb_classes, activation='softmax',
+                        activity_regularizer=activity_acol(c1, c2, c3, c4), name='L-1'))
 
     return model
