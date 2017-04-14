4e59d09628c763407a0db90e8a005a6b3536144d
ozcell <ozsel@USF-Gold-Wifi-226-23-180.laptops.usf.edu>
2017-04-14 16:46:17 -0400

    bugs related to model_in

--- phdwork/acol/trainings.py
+++ phdwork/acol/trainings.py
@@ -18,7 +18,7 @@ def train_with_parents(nb_parents, nb_clusters_per_parent,
                        nb_reruns, nb_epoch, nb_dpoints, batch_size,
                        test_on_test_set=True, update_c3=None,
                        return_model=False,
-                       save_after_each_rerun=False, verbose=1, model_in=None):
+                       save_after_each_rerun=None, verbose=1, model_in=None):
 
     #find the values of the dependent variables used inside the script
     nb_all_clusters = nb_parents*nb_clusters_per_parent
@@ -150,7 +150,7 @@ def train_with_parents(nb_parents, nb_clusters_per_parent,
         print_stats(verbose, 3, rerun_start=rerun_start, rerun_end=rerun_end,
                     nb_reruns=nb_reruns, rerun=rerun)
 
-        if save_after_each_rerun:
+        if save_after_each_rerun is not None:
             np.save(save_after_each_rerun + 'metrics.npy', metrics)
             np.save(save_after_each_rerun + 'acti.npy', (acti_train, acti_test))
 
@@ -165,7 +165,7 @@ def train_with_pseudos(nb_pseudos, nb_clusters_per_pseudo,
                        nb_reruns, nb_epoch, nb_dpoints, batch_size,
                        test_on_test_set=True, update_c3=None,
                        set_original_only=None, return_model=False,
-                       save_after_each_rerun=False, verbose=1, model_in=None):
+                       save_after_each_rerun=None, verbose=1, model_in=None):
 
     #find the values of the dependent variables used inside the script
     nb_all_clusters = nb_pseudos*nb_clusters_per_pseudo
@@ -294,7 +294,7 @@ def train_with_pseudos(nb_pseudos, nb_clusters_per_pseudo,
         print_stats(verbose, 3, rerun_start=rerun_start, rerun_end=rerun_end,
                     nb_reruns=nb_reruns, rerun=rerun)
 
-        if save_after_each_rerun:
+        if save_after_each_rerun is not None:
             np.save(save_after_each_rerun + 'metrics.npy', metrics)
             np.save(save_after_each_rerun + 'acti.npy', (acti_train, acti_test))
 
@@ -309,7 +309,7 @@ def train_semisupervised(nb_pseudos, nb_clusters_per_pseudo,
                          nb_reruns, nb_epoch, nb_dpoints, batch_size,
                          test_on_test_set=True, update_c3=None,
                          set_original_only=None, return_model=False,
-                         save_after_each_rerun=False, verbose=1, model_in=False):
+                         save_after_each_rerun=None, verbose=1, model_in=None):
 
     #X_train[0] and y_train[0] are used for unlabeled training
     #nb_labeled samples are chosen from X_train[1] and y_train[1]
@@ -460,7 +460,7 @@ def train_semisupervised(nb_pseudos, nb_clusters_per_pseudo,
         print_stats(verbose, 3, rerun_start=rerun_start, rerun_end=rerun_end,
                     nb_reruns=nb_reruns, rerun=rerun)
 
-        if save_after_each_rerun:
+        if save_after_each_rerun is not None:
             np.save(save_after_each_rerun + 'metrics.npy', metrics)
             np.save(save_after_each_rerun + 'acti.npy', (acti_train, acti_test))
 
@@ -553,7 +553,7 @@ def fit_pseudo(self, X, nb_pseudos, batch_size, nb_epoch,
             history_train.extend(history_test)
 
         if verbose:
-            progbar.add(1, values=values)
+            progbar.update(1, values=values)
 
     return history_train
 
@@ -606,7 +606,7 @@ def fit_pseudo_supervised(self, X, y, nb_classes, nb_pseudos, batch_size, nb_epo
             history_train.extend(history_test)
 
         if verbose:
-            progbar.add(1, values=values)
+            progbar.update(1, values=values)
 
     return history_train
 
