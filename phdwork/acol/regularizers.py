c27f41727260cdf3327a7f90a4224d30182e04f8
ozcell <ozsel@ozsel-mac.local>
2017-05-31 03:26:36 -0400

    how to calculate v for parent case

--- phdwork/acol/regularizers.py
+++ phdwork/acol/regularizers.py
@@ -31,10 +31,10 @@ class AcolRegularizer(Regularizer):
         n = K.shape(Z)[1]
 
         Z_bar = Z * K.cast(x>0., K.floatx())
-        #v = K.sum(Z_bar, axis=0).reshape((1,n))
+        v = K.sum(Z_bar, axis=0).reshape((1,n))
 
         U = K.dot(Z_bar.T, Z_bar)
-        v = Diag(U).reshape((1,n))
+        #v = Diag(U).reshape((1,n))
         V = K.dot(v.T, v)
 
         affinity = (K.sum(U) - Tr(U))/((n-1)*Tr(U))
